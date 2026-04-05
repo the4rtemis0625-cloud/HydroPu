db0621c58256a606f0df378b9e62b7fce3e653bf
John Michael Ortiz <the4rtemis0625@gmail.com>
2026-04-05 05:15:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7245934462-e949c",
+  "appId": "1:286132451521:web:aded21b3474b10013ebc40",
+  "apiKey": "AIzaSyDLjnspdxKYN42abQx4T9VuLDIh-_lyTac",
+  "authDomain": "studio-7245934462-e949c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "286132451521"
+};
